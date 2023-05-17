a9692561eb75ebafbea3e6500b6c4a783e4fabf6
kevinand11 <kevinfizu@gmail.com>
2023-05-17 02:02:10 +0100

    deployment file

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,24 +1,23 @@
 const fs = require('fs')
 
-const branchName = 'site-deployment'
-
 import('execa').then(({ execaSync }) => {
 	try {
-		execaSync('git', ['checkout', '-B', branchName])
+		execaSync('git', ['checkout', '-B', 'gh-pages'])
 		console.log('Building started...')
 		execaSync('yarn', ['generate'])
-		const folderName = "dist"
-		execaSync('cp', ['-r', './.github', folderName])
+		const folderName = 'dist'
+		execaSync('touch', [`${folderName}/.nojekyll`])
+		fs.writeFileSync(`${folderName}/CNAME`, 'tato.gg')
 		execaSync('git', ['--work-tree', folderName, 'add', '--all'])
-		execaSync('git', ['--work-tree', folderName, 'commit', '-m', branchName])
-		console.log(`Pushing to ${branchName}...`)
-		execaSync('git', ['push', 'origin', `HEAD:${branchName}`, '--force'])
+		execaSync('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
+		console.log('Pushing to gh-pages...')
+		execaSync('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
 		execaSync('rm', ['-r', folderName])
 		execaSync('git', ['checkout', '-f', 'main'])
-		execaSync('git', ['branch', '-D', branchName])
+		execaSync('git', ['branch', '-D', 'gh-pages'])
 		console.log('Successfully deployed, check your settings')
 	} catch (e) {
-		console.log(e.message)
-		process.exit(1)
+			console.log(e.message)
+			process.exit(1)
 	}
 })
